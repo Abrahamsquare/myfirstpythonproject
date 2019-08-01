bb12cb30339bb4ef7c77eb84bb1e2ef3703221ca
Abrahamsquare <53538773+Abrahamsquare@users.noreply.github.com>
2019-08-01 15:03:01 -0700

    Update unwrapper-tool.py

--- unwrapper-tool.py
+++ unwrapper-tool.py
@@ -1,18 +1,29 @@
 import os
-
+#get the current working directory 
 currentdir = os.getcwd();
+#Created a folder unwrapped in C:\current working directory\
+#NameError: name 'currdir' is not defined
+#changed currdir to currentdir
 fulloutputpath = currdir + '\\unwrapped\\';
+#fulloutputpath = currentdir + '\\unwrapped\\';
 
+#it will return true becuase C:\current working directory\unwrapped already exist
 if(os.path.exists(fulloutputpath) is False):
     os.mkdir(fulloutputpath);
 
+    
+#C:\Users\Senai\Desktop\expin\sample#1
 filetounwrap = input('Enter the full path to the file you want to unwrap:');
+
+#a null value
 all_lines = None
 
+#read all lines in the file(C:\Users\Senai\Desktop\expin\sample#1)
 if(os.path.exists(filetounwrap)):
     with open(filetounwrap,'r') as lines:
         all_lines = lines.readlines();
-    
+ 
+#writing to unwrapped.txt
     unwrapped = unwrap(all_lines);
     with open(fulloutputpath + 'unwrapped.txt','w') as clean:
         for line in unwrapped:
@@ -20,9 +31,12 @@ if(os.path.exists(filetounwrap)):
             clean.write('\n');
 
 #Implement
+#function named unwrap
+#I dont understand what the function is creating to do
 def unwrap(lines):
     cleaned = []
     for line in lines:
+        
         #implement here
     
     return cleaned;
